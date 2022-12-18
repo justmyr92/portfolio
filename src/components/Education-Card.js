816c4d70e30725f88205ce36be35c8d988d02b18
Justmyr <justmyrgutierrez92@gmail.com>
2022-12-18 17:25:31 +0800

    tangina mo jaja

--- src/components/Education-Card.js
+++ src/components/Education-Card.js
@@ -1,7 +1,9 @@
 const EducationCard = ({ education }) => {
     return (
         <div className="education-card">
-            <h4>{education.type}</h4>
+            <h4>
+                <i class="fa-solid fa-graduation-cap"></i> {education.type}
+            </h4>
             <p className="school">{education.school}</p>
             <p>{education.level}</p>
             <p>{education.year}</p>
